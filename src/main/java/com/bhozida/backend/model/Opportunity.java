ec0c12d73dc4fb50144646229fca3edf3b1c8f25
Denford Kambarami <dkambarami@gmail.com>
2020-05-04 15:26:29 +0000

    Optimised Models for Business Idea Entity Class

--- src/main/java/com/bhozida/backend/model/Opportunity.java
+++ src/main/java/com/bhozida/backend/model/Opportunity.java
@@ -12,7 +12,11 @@ import java.io.Serializable;
 @Component
 @Entity
 public class Opportunity implements Serializable {
-    @Id
+    /**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String businessName;
